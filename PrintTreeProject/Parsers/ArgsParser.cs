ae4c4fc34f5613a934a9bf21f8c5d36b5127382b
Ahmad Aden <ahmadx222x2gmail.com>
2025-07-22 22:15:57 +0300

    Add ArgsParser without Tester.

---
+++ PrintTreeProject/Parsers/ArgsParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintTreeProject.Parsers
+{
+    public class ArgsParser
+    {
+        public static Dictionary<string, string> Parse(string[] args)
+        {
+            var options = new Dictionary<string, string>();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+
+                // إذا الخيار يبدأ بـ "-" أو "--"
+                if (arg.StartsWith("-"))
+                {
+                    // هل له قيمة تالية؟ (وليست خيارًا جديدًا)
+                    if (i + 1 < args.Length && !args[i + 1].StartsWith("-"))
+                    {
+                        options[arg] = args[i + 1];
+                        i++; // تخطى القيمة
+                    }
+                    else
+                    {
+                        options[arg] = null; // بدون قيمة (flag فقط)
+                    }
+                }
+            }
+
+            return options;
+        }
+    }
+}
--- PrintTreeProject/Program.cs
+++ PrintTreeProject/Program.cs
@@ -10,7 +10,7 @@ namespace PrintTreeProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo World");
+            
         }
     }
 }
